296af5ad1a6dd40bafb97630de2aab99060a4748
krunal dhavle <krunaldhavle1999@gmail.com>
2022-11-28 16:36:09 +0000

    Added mysql query and logic of login page

--- src/MobileBankManagement/Transactions.java
+++ src/MobileBankManagement/Transactions.java
@@ -8,8 +8,15 @@ import java.awt.event.*;
  * @author krunal
  */
 public class Transactions extends JFrame implements ActionListener{
+         //global declaration
+         JButton deposit, withdraw,FastCash,Balance , accdet,pinch,ministatement,exit;
+         String cardnumber , pinnumber;
     
-        Transactions(){
+    
+        Transactions(String cardnumber , String pinnumber){
+        this.cardnumber = cardnumber;
+        this.pinnumber = pinnumber;
+        
         setLayout(null);
         setTitle("Transactions Menu");
         
@@ -19,76 +26,76 @@ public class Transactions extends JFrame implements ActionListener{
         text.setBounds(300,60,900,50);
         add(text);
         
-        JButton deposit = new JButton("Deposit Money");
+        deposit = new JButton("Deposit Money");
         deposit.setFont(new Font("Arial" , Font.BOLD , 28));
         deposit.setBounds(60, 200, 290, 60);
         deposit.setBackground(Color.white);
         deposit.setForeground(Color.green);
         deposit.setFocusPainted(false);
-        //deposit.addActionListener(this);
+        deposit.addActionListener(this);
         add(deposit);
 
-        JButton withdraw = new JButton("Withdraw Cash");
+        withdraw = new JButton("Withdraw Cash");
         withdraw.setFont(new Font("Arial" , Font.BOLD , 28));
         withdraw.setBounds(60, 300, 290, 60);
         withdraw.setBackground(Color.white);
         withdraw.setForeground(Color.green);
         withdraw.setFocusPainted(false);
-        //deposit.addActionListener(this);
+        withdraw.addActionListener(this);
         add(withdraw);
         
-        JButton FastCash = new JButton("Fast Cash");
+        FastCash = new JButton("Fast Cash");
         FastCash.setFont(new Font("Arial" , Font.BOLD , 28));
         FastCash.setBounds(60, 400, 290, 60);
         FastCash.setBackground(Color.white);
         FastCash.setForeground(Color.green);
         FastCash.setFocusPainted(false);
-        //deposit.addActionListener(this);
+        FastCash.addActionListener(this);
         add(FastCash);
         
-        JButton Balance = new JButton("Balance Enquiry");
+        Balance = new JButton("Balance Enquiry");
         Balance.setFont(new Font("Arial" , Font.BOLD , 28));
         Balance.setBounds(60, 500, 290, 60);
         Balance.setBackground(Color.white);
         Balance.setForeground(Color.green);
         Balance.setFocusPainted(false);
-        //deposit.addActionListener(this);
+        Balance.addActionListener(this);
         add(Balance);
         
-        JButton accdet = new JButton("Account Details");
+        accdet = new JButton("Account Details");
         accdet.setFont(new Font("Arial" , Font.BOLD , 28));
         accdet.setBounds(900, 200, 290, 60);
         accdet.setBackground(Color.white);
         accdet.setForeground(Color.green);
         accdet.setFocusPainted(false);
-        //deposit.addActionListener(this);
+        accdet.addActionListener(this);
         add(accdet);
         
-        JButton pinch = new JButton("Pin Change");
+        pinch = new JButton("Pin Change");
         pinch.setFont(new Font("Arial" , Font.BOLD , 28));
         pinch.setBounds(900, 300, 290, 60);
         pinch.setBackground(Color.white);
         pinch.setForeground(Color.green);
         pinch.setFocusPainted(false);
-        //deposit.addActionListener(this);
+        pinch.addActionListener(this);
         add(pinch);
         
-        JButton ministatement = new JButton("Mini Statement");
+        ministatement = new JButton("Mini Statement");
         ministatement.setFont(new Font("Arial" , Font.BOLD , 28));
         ministatement.setBounds(900, 400, 290, 60);
         ministatement.setBackground(Color.white);
         ministatement.setForeground(Color.green);
         ministatement.setFocusPainted(false);
-        //deposit.addActionListener(this);
+        ministatement.addActionListener(this);
         add(ministatement);
         
-        JButton exit = new JButton("Exit");
+        exit = new JButton("Exit");
         exit.setFont(new Font("Arial" , Font.BOLD , 28));
         exit.setBounds(900, 500, 290, 60);
         exit.setBackground(Color.white);
         exit.setForeground(Color.green);
         exit.setFocusPainted(false);
-        //deposit.addActionListener(this);
+        exit.addActionListener(this);
         add(exit);
             
         getContentPane().setBackground(Color.DARK_GRAY);
@@ -101,14 +108,16 @@ public class Transactions extends JFrame implements ActionListener{
         }
         
         public void actionPerformed(ActionEvent ae){
-            
+            if(ae.getSource() == exit){
+                System.exit(0);
+            }
         }
 
         
     
     
     public static void main(String args[]){
-        new Transactions();
+        new Transactions("" , "");
         
     }
     
